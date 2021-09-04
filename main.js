382015908c2678888b80688f7378cab333f782b1
Jeferson <89223785+jefersonernestho@users.noreply.github.com>
2021-09-03 20:00:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hola clase esta es mi pagina');
+/*scroll*/
+ScrollReveal().reveal('.showcase',{delay:100});
+ScrollReveal().reveal('.columnas',{ delay: 900 });
+ScrollReveal().reveal('.semifinal',{ delay: 1100 });
+ScrollReveal().reveal('.s1',{ delay: 500 });
+ScrollReveal().reveal('.s2',{ delay: 900 });
+ScrollReveal().reveal('.s3',{ delay: 1200 });
